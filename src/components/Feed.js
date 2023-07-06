764add3ed1cead8c83cc91b620e666ba16fe2405
Taha Ceken <taha-ceken@hotmail.com>
2023-07-06 19:32:19 +0300

    add post section of feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,29 @@
 import { SparklesIcon } from '@heroicons/react/24/outline';
 import Input from './Input';
+import Post from './Post';
 
 export default function Feed() {
+  const posts = [
+    {
+      id : '1',
+      name :"Recep Baltaş",
+      username : 'buckberi',
+      userImg: 'https://pbs.twimg.com/profile_images/1617949667731423232/8-z9wqd-_400x400.jpg',
+      img:"https://pbs.twimg.com/media/FxmWJhoXwAE3SFJ?format=jpg&name=900x900",
+      text:'Benim son zamanda gördüğüm şu ki; evrendeki hemen her şey Cumhur İttifaki aleyhine çalişiyor.',
+      timestamp:'2 hours ago'
+    },
+    {
+      id : '2',
+      name :"Recep Baltaş",
+      username : 'buckberi',
+      userImg: 'https://pbs.twimg.com/profile_images/1617949667731423232/8-z9wqd-_400x400.jpg',
+      img:"https://pbs.twimg.com/card_img/1675743363520184323/L2g7ppCY?format=jpg&name=small",
+      text:'Randi Vakfinın Artık Sürdürülmeyen 1 Milyon Dolar Para Ödülü Neyi Amaçladı?',
+      timestamp:'2 days ago'
+    }
+  ]
+
   return (
     <div className='xl:ml-[370px] border-x border-[#2f3336] xl:min-w-[576px] sm:ml-[80px] flex-grow max-w-xl'>
 
@@ -15,7 +37,9 @@ export default function Feed() {
         </div>
       {/* Input  */}
       <Input/>
-
+      {posts.map((post)=>(
+        <Post key={post.id} post={post}/>
+      ))}
     </div>
   )
 }
---
+++ src/components/Post.js
@@ -0,0 +1,46 @@
+import { EllipsisHorizontalIcon, ChatBubbleLeftIcon, TrashIcon, HeartIcon, ChartBarSquareIcon, ShareIcon, ArrowUpIcon, ArrowUpTrayIcon, ChartBarIcon } from '@heroicons/react/24/outline';
+
+
+export default function Post({post}) {
+  return (
+    <div className='flex p-3 cursor-pointer border-b border-[#2f3336]'>
+        {/* User Image */}
+
+        <img className='h-11 w-11 rounded-full mr-4' src={post.userImg} alt="user-image" />
+        
+        {/*  Right Side */}
+        <div>
+        {/* Header */}
+
+        <div className='flex items-start'>
+        {/* Post User Info */}
+        <div className='flex items-center'>
+        <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>{post.name}</h4>
+        <span className='text-sm sm:text-[15px] text-[#71767b] ml-1'>@{post.username} · </span>
+        <span className='text-sm hover:underline sm:text-[15px] text-[#71767b] ml-1' >{post.timestamp}</span>
+        </div>
+
+        {/* Dot Icon */}
+        <EllipsisHorizontalIcon className='-mt-1 ml-auto text-[#45484b] hover:text-[#3a9def] h-8 w-9 p-1 hoverEffect'/>
+        </div>
+
+        {/* Post Text */}
+        <p className='text-[#e7e9ea] mb-2 sm:text-[16x] text-[15px]'>{post.text}</p>
+
+        {/* Post Image */}
+        <img className='rounded-2xl mr-2 border border-[#2f3336] ' src={post.img} alt="post-image" />
+
+        {/* Icons */}
+
+        <div className='flex justify-between mr-20'>
+        <ChatBubbleLeftIcon className='text-[#71767b] h-9 w-9 hoverEffect p-2 hover:text-[#3a9def]'/>
+        <TrashIcon className='text-[#71767b] h-9 w-9 hoverEffect p-2 hover:text-[#d5186f]'/>
+        <HeartIcon className='text-[#71767b] h-9 w-9 hoverEffect p-2 hover:text-[#d5186f]'/>
+        <ChartBarIcon className='text-[#71767b] h-9 w-9 hoverEffect p-2 hover:text-[#3a9def]'/>
+        <ArrowUpTrayIcon className='text-[#71767b] h-9 w-9 hoverEffect p-2 hover:text-[#3a9def]'/>
+        </div>
+
+        </div>
+    </div>
+  )
+}
